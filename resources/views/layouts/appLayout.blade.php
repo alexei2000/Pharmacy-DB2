088ecbc66041fc21f418e1ee15eb8776b555737d
David Brito <39559632+davbrito@users.noreply.github.com>
2021-10-26 00:37:20 -0400

    Favicon y Logo

--- resources/views/layouts/appLayout.blade.php
+++ resources/views/layouts/appLayout.blade.php
@@ -19,7 +19,7 @@
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/lib/chosen/chosen.min.css">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
-
+    <link rel="shortcut icon" href="/images/favicon.ico">
     <style>
         .fadeIn,
         .card {
@@ -79,7 +79,7 @@
         <header id="header" class="header">
             <div class="top-left">
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="./"><img src="/images/logo.png" alt="Logo"></a>
+                    <a class="navbar-brand" href="./"><img style="height: 40px" src="/images/farmaplay.png" alt="Logo"></a>
                     <a class="navbar-brand hidden" href="./"><img src="/images/logo2.png" alt="Logo"></a>
                     <a id="menuToggle" class="menutoggle"><i class="fa fa-bars"></i></a>
                 </div>
