a3d5221ef02ef0806d38c811d0cad9ced18c38f5
ildebr <73916430+ildebr@users.noreply.github.com>
2021-10-26 21:32:30 -0400

    Add files via upload

--- resources/views/medicamento/show.blade.php
+++ resources/views/medicamento/show.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.appLayout')
 
 @section('content')
-    <a href="{{ route('medicinas.edit', $medicina->id) }}">editar</a>
+    <div class="container">
+        <div class="card">
+        <div class="card-body">
+        <a href="{{ route('medicinas.edit', $medicina->id) }}"><button class="btn">Editar</button></a>
     <div class="wrapper medicina">
-        <h1>{{ $medicina->name }}</h1>
+        <h1 class="box-title">{{ $medicina->name }}</h1>
         <p class="">{{ $medicina->principal_component }} </p>
         <p class="">{{ $medicina->monodrug }} </p>
        
@@ -15,21 +18,10 @@
         </form>
     </div>
     <a href="/medicinas" class="back">Back</a>
+        
+        </div>
+        </div>
+    </div>
 
-    <div class="container">
-    <div class="card">
-        <div class="card-body d-flex justify-content-between">
-            <h4 class="box-title">{{$medicina->name}}</h4>
-            <button onclick="document.location = '{{}}'" type="button"
-                class="btn btn-success btn-sm">
-                <span><i class="mr-2 fas fa-plus"></i></span>
-                Editar
-            </button>
-        </div>
-        <div class="card-body--">
-           
-            
-        </div>
-    </div> <!-- /.card -->
-</div>
+    
 @endsection
