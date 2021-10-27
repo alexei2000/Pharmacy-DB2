34afd103db44e1e3aa815051343aee336c11df7f
ildebr <73916430+ildebr@users.noreply.github.com>
2021-10-26 21:15:20 -0400

    Add files via upload

--- resources/views/medicamento/edit.blade.php
+++ resources/views/medicamento/edit.blade.php
@@ -3,39 +3,103 @@
 @section('content')
 
 <div class="wrapper crear-medicina">
-        <h1>Crear medicina</h1>
-        <form action="{{ route('medicinas.update', $medicina->id) }}" method="POST">
-            @csrf
-            <!-- <label for="laboratorio">Laboratorio</label>
-            <input type="text" id="laboratorio" name="laboratorio"> -->
 
-            <label for="name">Nombre</label>
-            <input type="text" id="name" name="name" value="{{ $medicina->name }}">
+        <form class="modal-dialog modal-lg fadeIn" action="{{ route('medicinas.update', $medicina->id)}}" method="POST">
+    @csrf
+    @method('PUT')
+    <div class="modal-content">
+        <div class="modal-header d-flex">
+            <h5 class="modal-title">Editar Medicina</h5>
+        </div>
+        <div class="modal-body">
+            <div class="form-row">
+                <div class="col">
+                    <div class="form-group">
+                        <label class=" form-control-label">Nombre</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-search-location"></i></span>
+                            </div>
+                            <input class="form-control" name="name" required value="{{ $medicina->name }}"> 
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <label class=" form-control-label">Componente Principal</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-search-location"></i></span>
+                            </div>
+                            <input class="form-control" name="principal_component" required value="{{ $medicina->principal_component }}">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="col">
+                    <div class="form-group">
+                        <label class="form-control-label">Monodroga</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-mail-bulk"></i></span>
+                            </div>
+                            <input type="text" class="form-control" name="monodrug" required value="{{ $medicina->monodrug }}">
+                        </div>
+                    </div>
+                </div>
 
-            <label for="principal_component">Componente Principal</label>
-            <input type="text" id="principal_component" name="principal_component" value="{{ $medicina->principal_component }}">
+                <div class="col">
+                    <div class="form-group">
+                        <label class=" form-control-label">Contenido</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-map-marker-alt"></i></span>
+                            </div>
+                            <input type="text" class="form-control" name="content" required value="{{ $medicina->content }}">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="col">
+                    <div class="form-group">
+                        <label class=" form-control-label">Unidad</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-at"></i></span>
+                            </div>
+                            <select name="unit" id="unit" value="{{ $medicina->unit }}">
+                                <option value="l">l</option>
+                                <option value="ml">ml</option>
+                                <option value="g">g</option>
+                                <option value="mg">mg</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
 
-            <label for="monodrug">Monodroga</label>
-            <input type="text" id="monodrug" name="monodrug" value="{{ $medicina->monodrug }}">
+                <div class="col">
+                    <div class="form-group">
+                        <label class=" form-control-label">Accion terapeutica</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-phone"></i></span>
+                            </div>
+                            <input type="tel" class="form-control" name="therapeutic_action" required value="{{ $medicina->therapeutic_action }}">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal-footer">
+            <input type="submit" class="btn btn-success" value="Crear" />
+        </div>
+    </div>
+</form>
 
-            <label for="content">Contenido</label>
-            <input type="text" id="content" name="content" value="{{ $medicina->content }}">
 
-            <label for="unit">Unidad</label>
-            <select name="unit" id="unit" value="{{ $medicina->unit }}">
-                <option value="l">l</option>
-                <option value="ml">ml</option>
-                <option value="g">g</option>
-                <option value="mg">mg</option>
-            </select>
-            
-            <label for="therapeutic_action">Accion Terapeutica</label>
-            <input type="text" id="therapeutic_action" name="therapeutic_action" value="{{ $medicina->therapeutic_action }}">
-            
-
-           
-            <input type="submit" value="Crear Medicina">
-        </form>
+        
     </div>
 
     @endsection
