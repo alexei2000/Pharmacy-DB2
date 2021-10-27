e7ae5ab2d51f52f9c7549caaca9cb43dbb373a01
ildebr <73916430+ildebr@users.noreply.github.com>
2021-10-26 21:04:09 -0400

    Add files via upload

--- resources/views/laboratories/create.blade.php
+++ resources/views/laboratories/create.blade.php
@@ -3,27 +3,69 @@
 @section('content')
 
 <div class="wrapper crear-medicina">
-        <h1>Crear laboratorio</h1>
-        <form action="/laboratorios" method="POST">
-            @csrf
-            <!-- <label for="laboratorio">Laboratorio</label>
-            <input type="text" id="laboratorio" name="laboratorio"> -->
+        
 
-            <label for="name">Nombre</label>
-            <input type="text" id="name" name="name">
+    <form class="modal-dialog modal-lg fadeIn" action="/laboratorios" method="POST">
+    @csrf
+    <div class="modal-content">
+        <div class="modal-header d-flex">
+            <h5 class="modal-title">Crear Laboratorio</h5>
+        </div>
+        <div class="modal-body">
+            <div class="form-row">
+                <div class="col">
+                    <div class="form-group">
+                        <label class=" form-control-label">Nombre</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-search-location"></i></span>
+                            </div>
+                            <input class="form-control" name="name" required>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <label class=" form-control-label">Dirección</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-search-location"></i></span>
+                            </div>
+                            <input class="form-control" name="address" required>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="col">
+                    <div class="form-group">
+                        <label class="form-control-label">Telefono</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-mail-bulk"></i></span>
+                            </div>
+                            <input type="text" class="form-control" name="phone_number" required>
+                        </div>
+                    </div>
+                </div>
 
-            <label for="address">Dirección</label>
-            <input type="text" id="address" name="address">
-
-            <label for="email">Email</label>
-            <input type="text" id="email" name="email">
-
-            <label for="phone_number">Telefono</label>
-            <input type="text" id="phone_number" name="phone_number">
-
-           
-            <input type="submit" value="Crear Laboratorio">
-        </form>
+                <div class="col">
+                    <div class="form-group">
+                        <label class=" form-control-label">Email</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-map-marker-alt"></i></span>
+                            </div>
+                            <input type="text" class="form-control" name="email" required>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        <div class="modal-footer">
+            <input type="submit" class="btn btn-success" value="Crear" />
+        </div>
     </div>
+</form>
 
     @endsection
